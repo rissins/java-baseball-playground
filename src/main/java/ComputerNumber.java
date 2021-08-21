6b69ea57aa2b38f64a7644e66630b7bd232d135b
rissins <tnalsqqq@naver.com>
2021-08-21 21:51:40 +0900

    feat(baseball): create randomNumber except overlap

--- src/main/java/ComputerNumber.java
+++ src/main/java/ComputerNumber.java
@@ -2,6 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerNumber {
+    public List<Integer> createRandomNumber() {
+        List<Integer> randomList = new ArrayList<Integer>();
+        List<Integer> resultList = new ArrayList<Integer>();
+        while (randomList.size() < 3) {
+            int number = (int)((Math.random()*9)+1);
+            if (!randomList.contains(number)) {
+                randomList.add(number);
+            }else{
+                continue;
+            }
+        }
+        return randomList;
+    }
+
+
 
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        ComputerNumber computerNumber = new ComputerNumber();
+        List<Integer> number = computerNumber.createRandomNumber();
+        System.out.println(number);
+    }
 }
